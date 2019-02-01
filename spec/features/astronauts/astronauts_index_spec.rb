c8dded0e290580e8cf39754f48613200785c8fa8
Julia Marco <marcootj@gmail.com>
2019-02-01 10:16:30 -0700

    Added test for user story 1

---
+++ spec/features/astronauts/astronauts_index_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  context 'when I visit /astronauts' do
+    it "displays a list of astronauts" do
+      neil = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+      buzz = Astronaut.create(name: "Buzz Aldrin", age: 55, job: "Pilot")
+
+      visit astronauts_path
+
+        within "#astronaut-#{neil.id}" do
+
+          expect(page).to have_content(neil.name)
+          expect(page).to have_content("Age: #{neil.age}")
+          expect(page).to have_content("Job: #{neil.job}")
+
+          expect(page).to_not have_content("Age: #{buzz.age}")
+          expect(page).to_not have_content("Job: #{buzz.job}")
+        end
+
+        within "#book-#{buzz.id}" do
+
+          expect(page).to have_content(buzz.name)
+          expect(page).to have_content("Job: #{buzz.job}")
+          expect(page).to have_content("Age: #{buzz.age}")
+
+          expect(page).to_not have_content("Age: #{neil.age}")
+          expect(page).to_not have_content("Job: #{neil.job}")
+        end
+
+      end
+
+  end
+end
