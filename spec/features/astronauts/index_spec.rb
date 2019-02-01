f4718469b9ee8bcf15d35f5b1beeeaa7402942c1
Julia Marco <marcootj@gmail.com>
2019-02-01 10:28:13 -0700

    Added test for user story 2

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -8,29 +8,38 @@ describe 'as a visitor' do
 
       visit astronauts_path
 
-        within "#astronaut-#{neil.id}" do
+      within "#astronaut-#{neil.id}" do
 
-          expect(page).to have_content(neil.name)
-          expect(page).to have_content("Age: #{neil.age}")
-          expect(page).to have_content("Job: #{neil.job}")
+        expect(page).to have_content(neil.name)
+        expect(page).to have_content("Age: #{neil.age}")
+        expect(page).to have_content("Job: #{neil.job}")
 
-          expect(page).to_not have_content("Age: #{buzz.age}")
-          expect(page).to_not have_content("Job: #{buzz.job}")
-        end
-
-        within "#astronaut-#{buzz.id}" do
-
-          expect(page).to have_content(buzz.name)
-          expect(page).to have_content("Job: #{buzz.job}")
-          expect(page).to have_content("Age: #{buzz.age}")
-
-          expect(page).to_not have_content("Age: #{neil.age}")
-          expect(page).to_not have_content("Job: #{neil.job}")
-          save_and_open_page
-          
-        end
-
+        expect(page).to_not have_content("Age: #{buzz.age}")
+        expect(page).to_not have_content("Job: #{buzz.job}")
       end
+
+      within "#astronaut-#{buzz.id}" do
+
+        expect(page).to have_content(buzz.name)
+        expect(page).to have_content("Job: #{buzz.job}")
+        expect(page).to have_content("Age: #{buzz.age}")
+
+        expect(page).to_not have_content("Age: #{neil.age}")
+        expect(page).to_not have_content("Job: #{neil.job}")
+      end
+    end
+
+    it 'shows the average age of all astronauts' do
+      neil = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+      buzz = Astronaut.create(name: "Buzz Aldrin", age: 55, job: "Pilot")
+
+      visit astronauts_path
+
+      within "#average_age" do
+
+        expect(page).to have_content(46)
+      end
+    end
 
   end
 end
